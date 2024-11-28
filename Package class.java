64f9b8490c4b62a1c5588f5208102a3ea052ab0a
damanic34 <damanicduffus@gmail.com>
2024-11-28 12:31:24 -0500

    Package class.java

--- Package class.java
+++ Package class.java
@@ -1,72 +1,78 @@
-public Class Package {
-    private float weight;
-    private String trackingNumber;
-    private String origin;
-    private String destination;
-    private String status;
-    LocalDateTime lastUpdated;
-    private int length;
-    private int witdth;
-}
+import java.time.LocalDateTime;
 
-public Package (float weight, String trackingNumeber, String origin, String Dest, String status, int length, int weight){
-        this.weight = weight;
+public class Package {
+    private String trackingNumber;  // Unique identifier
+    private String origin;          // Package origin
+    private String destination;     // Package destination
+    private String status;          // Current status (e.g., "In Transit")
+    private LocalDateTime lastUpdated;  // Timestamp of the last status update
+    private LocalDateTime estimatedDeliveryDate;  // Estimated delivery date
+    private double weight;          // Weight of the package
+    private double volume;          // Volume of the package
+
+    // Constructor
+    public Package(String trackingNumber, String origin, String destination, String status,
+                   double weight, double volume, LocalDateTime estimatedDeliveryDate) {
         this.trackingNumber = trackingNumber;
         this.origin = origin;
-        Dest = destination;
+        this.destination = destination;
         this.status = status;
-        this.length = length;
-        this.width = width;
+        this.weight = weight;
+        this.volume = volume;
+        this.estimatedDeliveryDate = estimatedDeliveryDate;
+        this.lastUpdated = LocalDateTime.now();  // Automatically sets the time of creation
+    }
+
+    // Getters and Setters
+    public String getTrackingNumber() {
+        return trackingNumber;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+        this.lastUpdated = LocalDateTime.now();  // Update timestamp
+    }
+
+    public LocalDateTime getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public LocalDateTime getEstimatedDeliveryDate() {
+        return estimatedDeliveryDate;
+    }
+
+    public void setEstimatedDeliveryDate(LocalDateTime estimatedDeliveryDate) {
+        this.estimatedDeliveryDate = estimatedDeliveryDate;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public String getDetails() {
+        return "Tracking Number: " + trackingNumber + "\n" +
+               "Origin: " + origin + "\n" +
+               "Destination: " + destination + "\n" +
+               "Status: " + status + "\n" +
+               "Weight: " + weight + " kg\n" +
+               "Volume: " + volume + " cubic meters\n" +
+               "Estimated Delivery Date: " + estimatedDeliveryDate + "\n" +
+               "Last Updated: " + lastUpdated;
+    }
 }
-
-//returns all package details 
-public String getDetails(){
-
-}
-
-//updates package status and refreshes lastUpdated field
-public String newStatus(){
-
-}
-
-public class packageService extends package{
-        private Map<>;
-}
-
-public void addPackage(Package package){
-
-}
-
-public String getPackageDetails(String trackingNumber){
-
-}
-
-public updatePackageStatus(String trackingNumber, String newStatus){
-
-}
-
-public calculateEstimatedCost(String trackingNumber){
-
-}
-public packageService(){
-
-}
-
-public 
-
-public class packageTracker extends package{
-    private packageService packageService;
-    private ScheduledExecutorService scheduler;
-
-
-}
-
-public packageTracker(){
-    
-}
-
-public String displayPackageDetails(String trackingNumber){
-
-}
-
-public autoRefreshStatus 
